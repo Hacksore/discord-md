749fc155a9603ad397f703d7cb4f9b7a03ce8c17
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-03 17:34:24 -0500

    Add heading links

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/static";
 
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
+import type { RehypePlugins } from "astro";
+
 // https://docs.astro.build/en/guides/markdown-content/#extending-markdown-config-from-mdx
 import tailwind from "@astrojs/tailwind";
 
@@ -10,14 +15,30 @@ export default defineConfig({
   trailingSlash: "never",
   output: "static",
   markdown: {
-    syntaxHighlight: "prism",
-    rehypePlugins: [],
-    gfm: true
+    shikiConfig: {
+      theme: "github-dark-dimmed",
+    },
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          content: {
+            type: "element",
+            tagName: "div",
+            properties: {
+              class: "anchor-link"
+            },
+          },
+        },
+      ],
+    ] as RehypePlugins,
   },
   adapter: vercel({
     webAnalytics: {
-      enabled: true
-    }
+      enabled: true,
+    },
   }),
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
