ed55e90af1e8c485da5b9278f42a19df14ca4bc1
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-03 14:36:47 -0500

    It works!

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/static";
 
 // https://docs.astro.build/en/guides/markdown-content/#extending-markdown-config-from-mdx
 export default defineConfig({
+  site: "https://discord-md.vercel.app",
+  trailingSlash: "never",
+  output: "static",
   markdown: {
     syntaxHighlight: "prism",
     rehypePlugins: [],
     gfm: true,
   },
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
